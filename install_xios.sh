e6eb7174db07d165bc431ecef5d2fba57a7774ee
Francesco Tucciarone <spam.ftcc@gmail.com>
2025-06-11 17:40:52 +0200

    Added install_xios.sh

--- install_xios.sh
+++ install_xios.sh
@@ -1,40 +1,9 @@
 # Let's base ourselves in the base directory
-ROOT=$HOME
+source install_params.sh
 
 # install what is needed
 sudo apt-get -y update
 sudo apt-get install -y openmpi-bin libmpich-dev libopenmpi-dev gcc g++ gfortran subversion libcurl4-openssl-dev wget make m4 git liburi-perl libxml2-dev
-
-# Echo where we work
-echo $ROOT
-
-# compilers
-export CC=/usr/bin/mpicc
-export CXX=/usr/bin/mpicxx
-export FC=/usr/bin/mpif90
-export F77=/usr/bin/mpif77
-
-# compiler flags (except for libraries)
-export CFLAGS="-O3 -fPIC"
-export CXXFLAGS="-O3 -fPIC"
-export F90FLAGS="-O3 -fPIC"
-export FCFLAGS="-O3 -fPIC"
-export FFLAGS="-O3 -fPIC"
-export LDFLAGS="-O3 -fPIC "
-
-# FLAGS FOR F90  TEST-EXAMPLES
-export FCFLAGS_f90="-O3 -fPIC "
-
-# Create some environment variables
-# directories
-export WORKDIR=$ROOT/nemo-deps/sources
-export INSTDIR=$ROOT/nemo-deps/installs
-export XIOSDIR=$ROOT/nemo-deps/XIOS
-mkdir -p $WORKDIR
-mkdir -p $INSTDIR
-mkdir -p $XIOSDIR
-
-
 
 
 #
